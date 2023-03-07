8d1ee4a70de5e767e9bb52c687829f1fed1e7daf
Moi1oM <hcloud0806@gmail.com>
2023-03-07 18:18:15 +0900

    [FEAT] MYSQL, MONGODB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useFindAndModify: false,
+      //useCreateIndex: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DBNAME,
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //CORS Setting
+  app.enableCors();
+
+  //SuccessInterceptor Setting
+  app.useGlobalInterceptors(new SuccessInterceptor());
+
+  //Validation Setting
+  app.useGlobalPipes(new ValidationPipe());
+
+  //Filter Setting
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
